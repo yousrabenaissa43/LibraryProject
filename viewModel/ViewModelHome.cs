55fcb6475d32e8f4c3750c88cd9f7c206f317ce6
yousrabenaissa43 <yousrabenaissa43@gmail.com>
2025-03-03 22:15:10 +0100

    added home page + general fixes

---
+++ viewModel/ViewModelHome.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LibraryProject.view;
+using System.Windows;
+
+namespace LibraryProject.ViewModel
+{
+    public partial class ViewModelHome : ObservableObject
+    {
+        [RelayCommand]
+        private void OpenBooks()
+        {
+            var booksWindow = new ViewBooks();
+            booksWindow.Show();
+
+        }
+
+        [RelayCommand]
+        private void OpenAuthors()
+        {
+            var authorsWindow = new ViewAuthors();
+            authorsWindow.DataContext = new ViewModelAuthors();
+            authorsWindow.Show();
+        }
+
+        [RelayCommand]
+        private void OpenLoans()
+        {
+            var loansWindow = new ViewLoans();
+            loansWindow.DataContext = new ViewModelLoans();
+            loansWindow.Show();
+        }
+
+        [RelayCommand]
+        private void OpenMembers()
+        {
+            var membersWindow = new ViewLibraryMembers();
+            membersWindow.DataContext = new ViewModelLibraryMembers();
+            membersWindow.Show();
+        }
+    }
+}
